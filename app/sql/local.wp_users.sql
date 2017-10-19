44d0dd8420c9d67c2ef5a729cb12621f61ec7d4a
Igor Buyanov <igor.buyanov@byroo.fi>
2017-10-19 09:48:56 +0300

    Set more generic settings

--- app/sql/local.wp_users.sql
+++ app/sql/local.wp_users.sql
@@ -2,4 +2,4 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_users` VALUES
-(1,"igor","$2y$10$9l3I5MOZAo0hYKHuEkc.Vu0nTH8DAyM3PP6X/OsYZkRQoyxmp7Olq","igor","dev-email@flywheel.local","","2017-10-13 08:28:41","",0,"igor");
+(1,"local-bedrock","$2y$10$QP41WKBEUdy9R2xxA67//.dsfay0yyA4t64/Lj2HKYZr9UX0tDF/.","local-bedrock","dev-email@flywheel.local","","2017-10-13 08:28:41","",0,"local-bedrock");
